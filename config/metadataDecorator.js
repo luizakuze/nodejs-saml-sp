9a27fd18d015cabd4b09fed7b8d8c33c72b238d7
luizakuze <luizakuze08@gmail.com>
2025-05-01 17:37:11 -0300

    refactor: substitir spssodescriptor por um novo

--- config/metadataDecorator.js
+++ config/metadataDecorator.js
@@ -10,128 +10,106 @@ module.exports = async function decorate(xml, {
 }) {
   const doc = await parseStringPromise(xml);
   const entityDescriptor = doc.EntityDescriptor;
-  const spsso = entityDescriptor.SPSSODescriptor[0];
 
-  // EntityDescriptor
+  // Atualiza atributos do EntityDescriptor
   entityDescriptor.$.entityID = entityID;
   entityDescriptor.$.ID = entityID.replace(/[^\w]/g, '_');
   entityDescriptor.$.cacheDuration = 'PT1H';
   entityDescriptor.$['xmlns:saml2'] = 'urn:oasis:names:tc:SAML:2.0:assertion';
 
-  // SPSSODescriptor atributos na ordem correta
-  spsso.$ = {
-    AuthnRequestsSigned: 'true',
-    WantAssertionsSigned: 'true',
-    protocolSupportEnumeration: 'urn:oasis:names:tc:SAML:2.0:protocol'
-  };
-
-  // Extensions
-  const extensions = {
-    'mdui:UIInfo': [{
-      '$': { 'xmlns:mdui': 'urn:oasis:names:tc:SAML:metadata:ui' },
-      'mdui:DisplayName': [{
-        _: uiInfo.displayName,
-        '$': { 'xml:lang': 'en' }
-      }],
-      'mdui:Description': [{
-        _: uiInfo.description,
-        '$': { 'xml:lang': 'en' }
-      }],
-      'mdui:InformationURL': [{
-        _: uiInfo.infoUrl,
-        '$': { 'xml:lang': 'en' }
-      }],
-      'mdui:PrivacyStatementURL': [{
-        _: uiInfo.privacyUrl,
-        '$': { 'xml:lang': 'en' }
-      }]
-    }],
-    'DiscoveryResponse': [{
-      '$': {
-        xmlns: 'urn:oasis:names:tc:SAML:profiles:SSO:idp-discovery-protocol',
-        Binding: 'urn:oasis:names:tc:SAML:profiles:SSO:idp-discovery-protocol',
-        Location: discoveryUrl,
-        isDefault: 'true',
-        index: '0'
-      }
-    }]
-  };
-
-  // Certificado formatado
+  // Formatando certificado (64 colunas)
   const formattedCert = certBase64.replace(/(.{64})/g, '$1\n');
 
-  // KeyDescriptors
-  const keyDescriptors = [
-    {
-      $: { use: 'signing' },
-      'ds:KeyInfo': [{
-        'ds:X509Data': [{
-          'ds:X509Certificate': [formattedCert]
-        }]
-      }]
+  // Construindo SPSSODescriptor na ordem correta
+  const spsso = {
+    $: {
+      AuthnRequestsSigned: 'true',
+      WantAssertionsSigned: 'true',
+      protocolSupportEnumeration: 'urn:oasis:names:tc:SAML:2.0:protocol'
     },
-    {
-      $: { use: 'encryption' },
-      'ds:KeyInfo': [{
-        'ds:X509Data': [{
-          'ds:X509Certificate': [formattedCert]
+    Extensions: [{
+      'mdui:UIInfo': [{
+        '$': { 'xmlns:mdui': 'urn:oasis:names:tc:SAML:metadata:ui' },
+        'mdui:DisplayName': [{
+          _: uiInfo.displayName,
+          '$': { 'xml:lang': 'en' }
+        }],
+        'mdui:Description': [{
+          _: uiInfo.description,
+          '$': { 'xml:lang': 'en' }
+        }],
+        'mdui:InformationURL': [{
+          _: uiInfo.infoUrl,
+          '$': { 'xml:lang': 'en' }
+        }],
+        'mdui:PrivacyStatementURL': [{
+          _: uiInfo.privacyUrl,
+          '$': { 'xml:lang': 'en' }
         }]
       }],
-      EncryptionMethod: [
-        { $: { Algorithm: 'http://www.w3.org/2009/xmlenc11#aes256-gcm' } },
-        { $: { Algorithm: 'http://www.w3.org/2009/xmlenc11#aes128-gcm' } },
-        { $: { Algorithm: 'http://www.w3.org/2001/04/xmlenc#aes256-cbc' } },
-        { $: { Algorithm: 'http://www.w3.org/2001/04/xmlenc#aes128-cbc' } }
-      ]
-    }
-  ];
-
-  // SingleLogoutService
-  const logoutLocation = entityID.replace(/\/saml2\/metadata\/?$/, '') + '/logout';
-  const singleLogoutServices = [{
-    $: {
-      Binding: 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
-      Location: logoutLocation
-    }
-  }];
-
-  // NameIDFormat
-  const nameIDFormat = ['urn:oasis:names:tc:SAML:2.0:nameid-format:persistent'];
-
-  // AssertionConsumerService
-  const acsLocation = entityID.replace(/\/saml2\/metadata\/?$/, '') + '/login/callback';
-  const acsList = [
-    {
-      $: {
-        index: '0',
-        isDefault: 'true',
-        Binding: 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
-        Location: acsLocation
+      'DiscoveryResponse': [{
+        '$': {
+          xmlns: 'urn:oasis:names:tc:SAML:profiles:SSO:idp-discovery-protocol',
+          Binding: 'urn:oasis:names:tc:SAML:profiles:SSO:idp-discovery-protocol',
+          Location: discoveryUrl,
+          isDefault: 'true',
+          index: '0'
+        }
+      }]
+    }],
+    KeyDescriptor: [
+      {
+        $: { use: 'signing' },
+        'ds:KeyInfo': [{
+          'ds:X509Data': [{
+            'ds:X509Certificate': [formattedCert]
+          }]
+        }]
+      },
+      {
+        $: { use: 'encryption' },
+        'ds:KeyInfo': [{
+          'ds:X509Data': [{
+            'ds:X509Certificate': [formattedCert]
+          }]
+        }],
+        EncryptionMethod: [
+          { $: { Algorithm: 'http://www.w3.org/2009/xmlenc11#aes256-gcm' } },
+          { $: { Algorithm: 'http://www.w3.org/2009/xmlenc11#aes128-gcm' } },
+          { $: { Algorithm: 'http://www.w3.org/2001/04/xmlenc#aes256-cbc' } },
+          { $: { Algorithm: 'http://www.w3.org/2001/04/xmlenc#aes128-cbc' } }
+        ]
       }
-    },
-    {
+    ],
+    SingleLogoutService: [{
       $: {
-        index: '1',
-        isDefault: 'false',
-        Binding: 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Artifact',
-        Location: acsLocation
+        Binding: 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+        Location: entityID.replace(/\/saml2\/metadata\/?$/, '') + '/logout'
       }
-    }
-  ];
+    }],
+    NameIDFormat: ['urn:oasis:names:tc:SAML:2.0:nameid-format:persistent'],
+    AssertionConsumerService: [
+      {
+        $: {
+          index: '0',
+          isDefault: 'true',
+          Binding: 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
+          Location: entityID.replace(/\/saml2\/metadata\/?$/, '') + '/login/callback'
+        }
+      },
+      {
+        $: {
+          index: '1',
+          isDefault: 'false',
+          Binding: 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Artifact',
+          Location: entityID.replace(/\/saml2\/metadata\/?$/, '') + '/login/callback'
+        }
+      }
+    ]
+  };
 
-  // Limpar e reinserir os elementos na ordem correta
-  delete spsso.Extensions;
-  delete spsso.KeyDescriptor;
-  delete spsso.SingleLogoutService;
-  delete spsso.NameIDFormat;
-  delete spsso.AssertionConsumerService;
-
-  // Inserir na ordem esperada
-  spsso.Extensions = [extensions];
-  spsso.KeyDescriptor = keyDescriptors;
-  spsso.SingleLogoutService = singleLogoutServices;
-  spsso.NameIDFormat = nameIDFormat;
-  spsso.AssertionConsumerService = acsList;
+  // Substitui SPSSODescriptor completo na ordem correta
+  entityDescriptor.SPSSODescriptor[0] = spsso;
 
   // Organization
   entityDescriptor.Organization = [{
